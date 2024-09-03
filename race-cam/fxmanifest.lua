8213ae292487d84966960b30039cb80be4cf14a4
Flinn <62859878+Save5Bucks@users.noreply.github.com>
2024-09-03 18:27:28 -0400

    Upload
    ...

---
+++ race-cam/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Save5Bucks'
+description 'Custom Camera Angles for Race Driving'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
