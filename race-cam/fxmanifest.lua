a253fb563cafef396526a65302a72708e1af9df1
Flinn <tim.e.flinn@gmail.com>
2024-09-04 17:37:42 -0400

    version checking

--- race-cam/fxmanifest.lua
+++ race-cam/fxmanifest.lua
@@ -3,9 +3,13 @@ game 'gta5'
 
 author 'Save5Bucks'
 description 'Custom Camera Angles for Race Driving'
-version '1.0.0'
+version '1.1'
 
 client_scripts {
     'config.lua',
     'client.lua'
+}
+
+server_scripts {
+    'server.lua'
 }
